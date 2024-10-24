03c727abc20ed43f9a7cdd3262de1d9c4d1e571c
lpm0831 <lpm0831@naver.com>
2024-10-24 15:03:45 +0900

    모듈 세팅 - data, domain, presentation, design system

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+}
+
+rootProject.name = "Multi_Module_Practice"
+include(":app")
+include(":domain")
+include(":presentation")
+include(":designsystem")
+include(":data")
